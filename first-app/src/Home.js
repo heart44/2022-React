f0931e9e53019a42005611f2a02bdc03e440d824
heart44 <gutt88656@gmail.com>
2022-08-24 12:09:36 +0900

    0824 3교시

--- first-app/src/Home.js
+++ first-app/src/Home.js
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom"
+
+import TodoApp from './TodoApp';
+import CoinTracker from './CoinTracker';
+import MovieApp from './MovieApp';
+
 export default function Home() {
     return (
         <div>
             <h1>Home</h1>
+            <Link to={TodoApp}>Todo</Link>
+            <Link to={CoinTracker}>Coin</Link>
+            <Link to={MovieApp}>Movie</Link>
         </div>
     )
 }
