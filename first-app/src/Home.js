3deecbfeccefeb5cd16e30a3e6fed71f72b53313
heart44 <gutt88656@gmail.com>
2022-08-24 12:12:05 +0900

    0824 edsdfsedf

--- first-app/src/Home.js
+++ first-app/src/Home.js
@@ -1,16 +1,12 @@
 import { Link } from "react-router-dom"
 
-import TodoApp from './TodoApp';
-import CoinTracker from './CoinTracker';
-import MovieApp from './MovieApp';
-
 export default function Home() {
     return (
         <div>
             <h1>Home</h1>
-            <Link to={TodoApp}>Todo</Link>
-            <Link to={CoinTracker}>Coin</Link>
-            <Link to={MovieApp}>Movie</Link>
+            <Link to='/todo'>Todo</Link>
+            <Link to='/coin'>Coin</Link>
+            <Link to='/movie'>Movie</Link>
         </div>
     )
 }
